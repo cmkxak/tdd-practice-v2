65ef035251eae66fbc42df7bbc8a698157e3e0cb
cmkxak <gcm466@naver.com>
2025-08-20 23:14:17 +0900

    feat : 포인트 사용 기능 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,5 +1,7 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.ErrorCode;
+import io.hhplus.tdd.HHPlusAppExcetion;
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.springframework.stereotype.Service;
@@ -41,5 +43,25 @@ public class PointService {
 
     private void updatePointHistory(UserPoint savedUserPoint, long amount, TransactionType type) {
         pointHistoryTable.insert(savedUserPoint.id(), amount, type, savedUserPoint.updateMillis());
+    }
+
+    public synchronized UserPoint use(long id, long amount) {
+        UserPoint currentUserPoint = findPointById(id);
+
+        validateEnoughBalance(currentUserPoint.point(), amount);
+
+        long remainingPoint = currentUserPoint.point() - amount;
+
+        UserPoint savedUserPoint = userPointTable.insertOrUpdate(id, remainingPoint);
+
+        updatePointHistory(savedUserPoint, amount, TransactionType.USE);
+
+        return savedUserPoint;
+    }
+
+    private void validateEnoughBalance(long memberBalance, long amount) {
+        if (memberBalance < amount) {
+            throw new HHPlusAppExcetion(ErrorCode.INVALID_BALANCE.getErrorResponse());
+        }
     }
 }
