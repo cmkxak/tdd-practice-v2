d8a3db0ef920d3be83ac050d9574fcaa7ee2da5a
cmkxak <gcm466@naver.com>
2025-08-20 23:12:03 +0900

    feat : 포인트 조회 기능 구현

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,24 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class PointService {
+
+    private final PointHistoryTable pointHistoryTable;
+    private final UserPointTable userPointTable;
+
+    public PointService(PointHistoryTable pointHistoryTable, UserPointTable userPointTable) {
+        this.pointHistoryTable = pointHistoryTable;
+        this.userPointTable = userPointTable;
+    }
+
+
+    public UserPoint findPointById(long id) {
+        return userPointTable.selectById(id);
+    }
+
+}
